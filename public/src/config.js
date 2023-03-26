dd0d88d1f2e676d742fbd04393010838703dadcb
Giarlan Santana <giarlansilva@yahoo.com>
2023-03-25 21:06:32 -0300

    REBUILD PROJECT STRUCTURE
       *Build - It was necessary to rebuild the project structure because of some basic configurations required to connect it to Firebase services

---
+++ public/src/config.js
@@ -0,0 +1,12 @@
+  const firebaseConfig = {
+    apiKey: "AIzaSyCkUpD_DmzuwbUh8-Aedxrw38pTcF3_MV4",
+    authDomain: "employment-agency-44d5e.firebaseapp.com",
+    projectId: "employment-agency-44d5e",
+    storageBucket: "employment-agency-44d5e.appspot.com",
+    messagingSenderId: "514273304212",
+    appId: "1:514273304212:web:d0411fd2749583b41e5e81",
+    measurementId: "G-TQBVJDB17E"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
